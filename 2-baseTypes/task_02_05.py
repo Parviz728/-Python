7f9d8428936163f93214bb60b19fdb2402f719dc
Parviz728 <pnorov5@gmail.com>
2023-11-02 17:11:40 +0300

    2 solved

--- 2-baseTypes/task_02_05.py
+++ 2-baseTypes/task_02_05.py
@@ -115,54 +115,33 @@ salary = [
     (4, '2021-01-01', 'bonus', 7000),
 ]
 
-import statistics
 from collections import defaultdict
 
-# переопределяем поведение statistics.mean на случай попадания пустого списка
-def mean(lst):
-    if lst:
-        return statistics.mean(lst)
-    return None
+report = defaultdict(dict)
 
-report = {}
-report["empID"] = [i[0] for i in employees]
-Emp = {}
-for mainID in report['empID']:
-    Emp[mainID] = defaultdict(list)
-
-for id in Emp:
-    for empID, date, salaryType, amount in salary:
-        if empID == id:
-            Emp[id][salaryType].append(amount)
-
-def noDuplicatesdict():
-    """
-    Формирует словарь где у каждого айдишника есть список его почт
-        
-    :return: dict 
-    """
-    was = set()
-    res = defaultdict(list)
-    for i in employees:
-        ID = i[0]
-        count = 0
-        for id, empID, email in emails:
-            if empID == ID:
-                count += 1
-                if email not in was:
-                    res[empID].append(email)
-                was.add(email)
-        if count == 0: # если ни разу мы не провалились в if на 150 строчке, значит у этого сотрудника нет почты
-            res[ID].append(None)
-    return res
-
-report["FIO"] = [f"{name} {surname} {fatherName}" for id, name, surname, fatherName in employees]
-report["avg_salaries"] = [mean(value["salary"]) for value in Emp.values()] # Средняя зарплата
-report["avg_bonuses"] = [mean(value["bonus"]) for value in Emp.values()] # Средние бонусы
-report["Email"] = noDuplicatesdict()
-ans = {}
-for empID, emails in report["Email"].items():
-    for email in emails:
-        ans[email] = [report['empID'][empID - 1], report['FIO'][empID - 1], report['avg_salaries'][empID - 1], report['avg_bonuses'][empID - 1]]
-for key, value in ans.items():
-    print(*value, key)  
+for empID, surname, name, fatherName in employees:
+    report[empID]["FIO"] = f"{surname} {name} {fatherName}"
+    report[empID]["salary"] = 0
+    report[empID]["count_salary"] = 0
+    report[empID]["bonus"] = 0
+    report[empID]["count_bonus"] = 0
+    report[empID]["Emails"] = set()
+    
+for id, empID, email in emails:
+    report[empID]["Emails"].add(email)
+    
+for empID, date, salaryType, amount in salary:
+    report[empID][salaryType] += amount
+    report[empID][f'count_{salaryType}'] += 1
+    
+for key, dict_value in report.items():
+    id = key
+    FIO = dict_value['FIO']
+    avg_salary = 0 if dict_value['count_salary'] == 0 else dict_value['salary'] / dict_value['count_salary']
+    avg_bonus = 0 if dict_value['count_bonus'] == 0 else dict_value['bonus'] / dict_value['count_bonus']
+    if dict_value['Emails']:
+        for email in dict_value['Emails']:
+            print(f"{id}, {FIO}, {avg_salary}, {avg_bonus}, {email}")
+    else:
+        print(f"{id}, {FIO}, {avg_salary}, {avg_bonus}, {None}")
+    
